4451f05d86821188ff9d4e1650defb59275afec4
Arthur Zachary <124051933+K0ola@users.noreply.github.com>
2025-09-15 10:55:02 +0200

    ♦️ + bases front

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 
 import s from "./Home.module.scss";
+import { FaEdit } from "react-icons/fa";
+
 
 function Home() {
     
   return (
-    <div>
-        <h1>Home Page</h1>
-    </div>
+    <section className={s.app}>
+      <div className={s.header}>
+
+      </div>
+      <div className= {s.container}>
+        <div className={s.contactsList}>
+
+            <div className={s.contact}>
+              <div className={s.contactIllu}>
+                
+              </div>
+              <div className={s.contactInfos}>
+                  <h1 className={s.name}>John Doe</h1>
+                  <h2 className={s.phoneNumber}>+33672395977</h2>
+              </div>
+              <FaEdit className={s.EditBTN}/>
+            </div>
+
+        </div>
+      </div>
+    </section>
     )
 }
 
